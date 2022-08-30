f8dfc8fe8da1abc94c41907fe1a9472162941fb0
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-08-30 15:56:06 +0430

    created a page for new meetup in pages

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,10 @@
+import NewMeetupForm from '../../components/meetups/NewMeetupForm';
+import Layout from '../../components/layout/Layout';
+
+export default function Home() {
+  return (
+    <Layout>
+      <NewMeetupForm />
+    </Layout>
+  );
+}
