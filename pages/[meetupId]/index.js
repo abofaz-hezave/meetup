f20775eb9d1410b547eb118e268ad7dad4418b44
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-09-01 18:24:38 +0430

    created meetup detail page with dynamic route in pages

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      id="m1"
+      title="Austia Meetup"
+      image="https://th-thumbnailer.cdn-si-edu.com/Ejt5akJFA2JjER12jJwUx-kiJ1U=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/03/25/03251376-be81-470c-ac01-a15130b3e9b0/kunst889.jpg"
+      address="Austia, Vienna"
+      description="description"
+    />
+  );
+}
+
+export default MeetupDetails;
