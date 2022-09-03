4fb4fb2f853fb44a33da5d1cfb278919bb75dba0
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2022-09-03 16:48:01 +0430

    updated new-meetup to call API on submit in pages

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,5 +1,20 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 export default function Home() {
-  return <NewMeetupForm />;
+  const router = useRouter();
+
+  async function addMeetup(enteredMeetup) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetup),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+    router.push('/');
+  }
+  return <NewMeetupForm onAddMeetup={addMeetup} />;
 }
