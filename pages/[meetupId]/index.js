18e2a94c544ef0a519a66c5147192d9bfca50285
Abolfazl-Khan <abolfazlkhan.m@gmail.com>
2023-02-12 22:32:34 +0330

    updated meetup detail page to read the data from database

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,48 @@
+import { MongoClient, ObjectId } from 'mongodb';
 import MeetupDetail from '../../components/meetups/MeetupDetail';
+import { databaseConnectionString } from '../../lib/constants';
 
-function MeetupDetails() {
-  return (
-    <MeetupDetail
-      id="m1"
-      title="Austia Meetup"
-      image="https://th-thumbnailer.cdn-si-edu.com/Ejt5akJFA2JjER12jJwUx-kiJ1U=/1000x750/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/03/25/03251376-be81-470c-ac01-a15130b3e9b0/kunst889.jpg"
-      address="Austia, Vienna"
-      description="description"
-    />
-  );
+function MeetupDetails({ selectedMeetup }) {
+  return <MeetupDetail {...selectedMeetup} />;
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(databaseConnectionString);
+
+  const db = client.db();
+
+  const meetupCollection = db.collection('meetup-collection');
+  const meetups = await meetupCollection.find({}, { _id: 1 }).toArray();
+
+  return {
+    fallback: false,
+    paths: meetups.map((meetupItem) => ({
+      params: { meetupId: meetupItem._id.toString() },
+    })),
+  };
+}
+
+export async function getStaticProps(ctx) {
+  const meetupId = ctx.params.meetupId;
+  const client = await MongoClient.connect(databaseConnectionString);
+
+  const db = client.db();
+
+  const meetupCollection = db.collection('meetup-collection');
+  const selectedMeetup = await meetupCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+  client.close();
+  return {
+    props: {
+      selectedMeetup: {
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image: selectedMeetup.image,
+        address: selectedMeetup.address,
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
